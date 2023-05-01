f339d5fc121898a92f865ecfe56fec3f3a9ff4c5
Emon <emon.mhk69@gmail.com>
2023-05-02 02:26:47 +0600

    all shefs data making done with cors

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
 const port  =  process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("thai master chefs api is running");
 });
+
+const shefs = require("./data/shefs.json");
+app.get("/shefs" , (req, res)=>{
+    res.send(shefs);
+})
 
 app.listen(port , ()=>{
     console.log(`thai master api is running on ${port}`);
